50dc70b2bffc3c9e29a94a24daf34f8557f1d53b
rishnegi7711 <rishnegi28@gmail.com>
2026-02-09 11:23:43 +0530

    Added basic checks if the node_modules folder exists in the directory

--- bin/cleandeps.js
+++ bin/cleandeps.js
@@ -1,26 +1,45 @@
 #!/usr/bin/env node
 
+const fs = require("fs");
+const path = require("path");
 
-const fs = require('fs');
-const path = require('path');
+const cwd = process.cwd();
+const pkgPath = path.join(cwd, "package.json");
+const nodeModulesPath = path.join(cwd, "node_modules");
 
-const cwd = process.cwd()
-const pkgPath = path.join(cwd,'package.json');
-
-function fileExists( filePath) {
-    try{
+function fileExists(filePath) {
+  try {
     fs.accessSync(filePath, fs.constants.F_OK);
-    return true
-    }catch{
-        return false
-    }
+    return true;
+  } catch {
+    return false;
+  }
 }
 
-if(!fileExists(pkgPath)){
-    console.error("❌ CleanDeps: No package.json found in this folder");
-    console.error("➡️ Run this command inside a Node project(where package.json exists).")
-    process.exit(1);
+if (!fileExists(pkgPath)) {
+  console.error("❌ CleanDeps: No package.json found in this folder");
+  console.error(
+    "➡️ Run this command inside a Node project(where package.json exists).",
+  );
+  process.exit(1);
 }
 
 console.log("✅ Found package.json");
 console.log(`📁 Project: ${cwd}`);
+
+if (!fs.existsSync(nodeModulesPath)) {
+  console.log("Node modules not found");
+  process.exit(1);
+}
+console.log("node modules found");
+
+if (!fs.statSync(nodeModulesPath).isDirectory()) {
+  console.error("This is not a valid directory");
+  process.exit(1);
+}
+console.log("correct path");
+if (!path.basename(nodeModulesPath) === "node_modules") {
+  console.error("Not a valid path");
+  process.exit(1);
+}
+console.log("correct path2");
